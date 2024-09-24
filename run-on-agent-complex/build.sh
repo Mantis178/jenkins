c3a5e394a3b0cb75d8b9a1f582af9c4ab2a165b2
Dor Amar <doramar5555@gmail.com>
2024-09-24 23:22:29 +0530

    New branch

--- run-on-agent-complex/build.sh
+++ run-on-agent-complex/build.sh
@@ -1,44 +1,44 @@
 #!/bin/bash
 
 # Function to check if Docker is installed
-check_docker_installed() {
-    if ! command -v docker &> /dev/null; then
-        echo "Docker is not installed. Installing Docker..."
-        install_docker
-    else
-        echo "Docker is already installed."
-    fi
-}
+# check_docker_installed() {
+#     if ! command -v docker &> /dev/null; then
+#         echo "Docker is not installed. Installing Docker..."
+#         install_docker
+#     else
+#         echo "Docker is already installed."
+#     fi
+# }
 
-install_docker() {
-    sudo yum update -y
-    sudo yum install -y docker
-    sudo service docker start
-    sudo usermod -aG docker ec2-user
-    newgrp docker
+# install_docker() {
+#     sudo yum update -y
+#     sudo yum install -y docker
+#     sudo service docker start
+#     sudo usermod -aG docker ec2-user
+#     newgrp docker
 
-    if command -v docker &> /dev/null; then
-        echo "Docker installed successfully."
-    else
-        echo "Docker installation failed."
-        exit 1
-    fi
-}
+#     if command -v docker &> /dev/null; then
+#         echo "Docker installed successfully."
+#     else
+#         echo "Docker installation failed."
+#         exit 1
+#     fi
+# }
 
-build_and_run_docker() {
-    ls
-    IMAGE_NAME="my-docker-image"
+# build_and_run_docker() {
+#     ls
+
+    IMAGE_NAME="my-first-application"
 
     sudo docker build -t $IMAGE_NAME ./run-on-agent-complex
-
     echo "Running the Docker container..."
     sudo docker run --rm $IMAGE_NAME
-}
+# }
 
-# Main script execution
-echo "Checking if Docker is installed..."
-check_docker_installed
+# # Main script execution
+# echo "Checking if Docker is installed..."
+# check_docker_installed
 
-echo "Building and running Docker container..."
-build_and_run_docker
+# echo "Building and running Docker container..."
+# build_and_run_docker
 
